a556e8ed35fc3ac9c641cc24533678cde557dc11
Christiano0407 <placebo0704@gmail.com>
2020-11-10 09:54:19 -0600

    play

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+const video = document.querySelector(`video`);
+const button = document.querySelector("button");
+
+function MediaPlayer(config) {
+    this.media = config.el;
+}
+
+MediaPlayer.prototype.play = function() {
+    this.media.play();
+}
+const player = new MediaPlayer({el:video});
+button.onclick = () => player.play();
+//video.play();
