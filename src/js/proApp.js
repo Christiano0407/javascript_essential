88c7bee60985e98da940f95233de02ec1512ac2f
Christiano0407 <placebo0704@gmail.com>
2020-11-11 09:53:34 -0600

    comparadores

--- src/js/proApp.js
+++ src/js/proApp.js
@@ -37,3 +37,41 @@ personas = persona1.concat(apellido1);
 console.log(personas); */
 
 //OPERADORES
+/* let num = 10;
+
+function numero() {
+    if (num === 10){
+    console.log("Son apenas 10")
+    }else {
+    console.log("No sabes sumar")
+    }
+
+    for( let i = 0; i <= num; i++ ){
+        console.log(i)
+    }
+}
+numero(); */
+//Booleano (true and false)
+/* let numi = 10;
+let num2 = 5;
+console.log(numi == num2); */
+let num1 = 10;
+let num2 = 5;
+//&&
+/* if(num1 === 10 && num2 === 5) {
+    console.log("Son iguales");
+}else {
+    console.log("No lo son");
+} */
+// ||
+/* if(num1 === 10 || num2 === 5) {
+    console.log("Son iguales");
+}else {
+    console.log("No lo son");
+} */
+// !
+/* if(num1 =! 20) {
+    console.log("Son iguales");
+}else {
+    console.log("No lo son");
+} */
