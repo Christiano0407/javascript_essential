0c1cb443826a2b05fecf6361ac1f984cf66fccdc
Christiano0407 <placebo0704@gmail.com>
2020-11-11 09:23:08 -0600

    one-js

---
+++ src/js/proApp.js
@@ -0,0 +1,39 @@
+/*  let nombre = prompt("Nombre?")
+
+if (nombre === "chris") {
+    console.log("Es mi novio");
+}else {
+    console.log("No lo conozco");
+}
+console.log(nombre);  */
+//valor de asignación
+/* let num = 10;
+if (num === 10) {
+    console.log(num += 5);
+}else {
+    console.log("No hay números");
+}
+console.log(num); */
+/* let numero = 20;
+if(numero === 20) {
+    document.write(numero += 10);
+}else {
+    document.write("No sé sumar");
+} */
+//Concatenar
+/* let hombre = {
+    nombre: "Chris",
+    edad: 25,
+}
+let mujer = {
+    nombre: "Alma",
+    edad: 24,
+}
+console.log(`Yo soy ${hombre.nombre}`, `Y, ella se llama: ${mujer.nombre}`); */
+//método: .concat
+/* let persona1 = "Mauricio";
+let apellido1 = "Saínz";
+personas = persona1.concat(apellido1);
+console.log(personas); */
+
+//OPERADORES
