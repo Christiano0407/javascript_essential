d9600c3113d90030660a6cf31aec90d118c706c5
Christiano0407 <placebo0704@gmail.com>
2020-11-12 08:30:29 -0600

    js

---
+++ src/js/appTwo.js
@@ -0,0 +1,45 @@
+//In line comments 
+//Data Types and Variables (undefined, null(nada-valor nada), boolean(true o false), string(cadena de texto), symbol, number, object(Puede almacenar clave-valor)).
+// Var (No es muy recomendada porque, la lee todo el progama).
+//let(local) y const(valor no se mueve) (scope).
+
+//Variables w/Assigment Operators(+,=,-,*...etc)
+/* let a = 9; */
+
+//DECLARATIONS:
+/* var studVar; */
+//ASSIGMENTS:
+/* studVar = 10; */
+//numbers
+/* let num;
+num = 10;
+console.log(num); */
+//increment Number (++)
+/* let num = 10;
+console.log(num += 5); */
+//Decrement(- o --)
+//
+/* let rema;
+rema = 11 % 3;
+console.log(rema); */
+//+=, -=, ++, --, incrementan el mismo valor. 
+/* let a = 10;
+a += 12;
+console.log(a);
+let b = 25;
+b -= 22;
+console.log(b); 
+let c = 2;
+c *= 2;
+console.log(c);*/
+
+//Variable w/ string (+ +, ${})
+/* let firstName = "Alan";
+console.log(`Hola, yo soy ${firstName}`);
+let divNumber = {
+    name: "Luisa",
+    age: 25,
+    country: "Canadá",
+}
+console.log(`hOLA!, yo soy ${divNumber.name}, y tengo ${divNumber.age}. Soy de ${divNumber.country}`);
+ */
