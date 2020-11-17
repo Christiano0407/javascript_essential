ff93ccc548ebd7eea63a51e1101e9ecf04bc3d5c
Christiano0407 <placebo0704@gmail.com>
2020-11-17 12:20:17 -0600

    complex-obj

---
+++ src/js/appFour.js
@@ -0,0 +1,44 @@
+//Delete Properties 
+/* let dog = {
+    name: "Dog",
+    age: 4,
+    legs: 4,
+}
+delete dog.age;
+console.log(dog); */
+
+//Obj/array/lookup
+
+/* phoneticLookup = () => {
+
+    let result = "";
+    let lookup = {
+        alpha: "Adams",
+        bravo: "Boston",
+        charlie: "Chicago",
+        delta: "Denver",
+        echo: "Easy",
+        foxtrot: "Frank",
+    };
+    result = lookup;
+    //return result;
+    console.log(result);
+    //console.log(result);
+}
+phoneticLookup(); */
+/* let myObj = {
+    alpha: "Adams",
+    bravo: "Boston",
+    charlie: "Chicago",
+};
+
+checkObj = (checkProp) => {
+  if (myObj.hasOwnProperty(checkProp)) {
+      return myObj[checkProp];
+  }else {
+      return "Not Found"
+  }
+};
+console.log(checkObj("Hello")); */
+
+//Manipulating Complex Object:
