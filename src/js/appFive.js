516ed349c2f037ecb64f5e931dc4d609107f89ab
Christiano0407 <placebo0704@gmail.com>
2020-11-19 08:18:03 -0600

    contacts

--- src/js/appFive.js
+++ src/js/appFive.js
@@ -12,3 +12,54 @@
 let product = multiPlay([[1,2],[3,4],[5,6,7]]);
 console.log(product); */
 // = 29 (1 + 28)
+//Do..While Loop 
+/* let myArray = [];
+let i = 10;
+
+ do {
+  myArray.push(i);
+  i++;
+} while(i < 5)
+console.log(i, myArray); */
+//Condición (11), y matriz 10.
+/* let contacts = [
+  {
+    "firstName": "Akira", 
+    "lastName": "Laine", 
+    "number": "004",
+    "likes": ["Pizza", "Coding", "Brownie Points"]
+  },
+  {
+    "firstName": "Harry", 
+    "lastName": "Potter", 
+    "number": "005",
+    "likes": ["Hogwarts", "Magic", "Hagrid"]
+  },
+  {
+    "firstName": "Sherlock", 
+    "lastName": "Holmes", 
+    "number": "006",
+    "likes": ["Intriguins", "Violin", "Cuestions"]
+  },
+  {
+    "firstName": "Kristian", 
+    "lastName": "Vos", 
+    "number": "007",
+    "likes": ["Javascript", "Gaming", "Foxes"]
+  }
+]; */
+
+/* lookProfile = (name, prop) => {
+  for(let i = 0; i < contacts.length; i++) {
+    if(contacts[i].firstName === name) {
+        return contacts[i][prop] || "No Property";
+    }else {
+       console.log("Nothing");
+    }
+  }
+  return "No such contacts"
+}
+let data = lookProfile("Akira", "likes");
+console.log(data); */
+
+//random (números aleatorios):
