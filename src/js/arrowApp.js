56bb39607cbc24d938c1a8ad55abde00f03c69b3
Christiano0407 <placebo0704@gmail.com>
2020-11-20 08:14:40 -0600

    prototype

--- src/js/arrowApp.js
+++ src/js/arrowApp.js
@@ -1,5 +1,19 @@
 //Puedes pasar (concatenar) argumentos a funciones.
 //Método concat().
-let myConcat = (arr1, arr2) => arr1.concat(arr2);
+/* let myConcat = (arr1, arr2) => arr1.concat(arr2);
 
-console.log(myConcat([1,2], [3,4,5]));
+console.log(myConcat([1,2], [3,4,5])); */
+
+//2)
+//Método filter()
+//isInteger()
+//Number.isInteger() = Dteremina si el valor pasado es de tipo entero.
+//.map() = Crea un nuevo Array con los resultados de la llamada a la función indicada a cada uno de sus elementos. 
+/* const realNumber = [4, 5, 6, -9.8, 3.14, 42, 6, 8.34, -2 ];
+const squareList = (arr) => {
+ 
+    const squareIntegers =  arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x);
+    return squareIntegers;
+}
+const squareIntegers = squareList(realNumber);
+console.log(squareIntegers); */
