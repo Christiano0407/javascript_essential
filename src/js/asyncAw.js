b682225f5db9eadffd8880b6742682b5187084d4
Christiano0407 <placebo0704@gmail.com>
2020-11-23 09:03:54 -0600

    Async

---
+++ src/js/asyncAw.js
@@ -0,0 +1,30 @@
+//Callback = Promeses = Generators = Async / Await.
+
+/* num = () => {
+ let numeros = 20;
+ if(numeros === 20) {
+     console.log("Soy un número.");
+ }else {
+     console.log("No es ciero");
+ }
+}
+num(); */
+/* nombres = (nom1, nom2) => {
+  console.log(`Hola, yo soy ${nom1}, y mi esposa es ${nom2}`);
+}
+nombres("Mauricio", "Sadri"); */
+/* person = (pers1 = "Lizbeth", pers2 = "Natalia") => {
+   console.log(`Yos soy ${pers1}, y mi hermana es ${pers2}.`);
+}
+person(); */
+/* nombres = () => {
+    let names = {name: "Luisa", edad: 29};
+ console.log(names.name);
+}
+nombres(); */
+
+//función anónima 
+/* const anonima = (a = "Liz", b = "CDMX") => {
+  console.log(`Hola yo soy ${a}, y soy de ${b}`);
+}
+anonima(); */
