842461ace7275beb2a2373a49bd2afe4220ec30d
Christiano0407 <placebo0704@gmail.com>
2020-11-25 09:34:07 -0600

    destructuración

---
+++ src/js/projectApp.js
@@ -0,0 +1,22 @@
+//Destructuring / Destructuración:
+
+//ARRAY:
+/* const number = [1,2,3,4,5];
+//Sin destructurar:
+let uno = number[0],
+    dos = number[1],
+    tres = number[2];
+console.log(uno,dos, tres);
+
+//Destructuración:
+const [one, two, three, four] = number;
+console.log(one, two, three, four); */
+
+//OBJECT:
+/* let person = {
+    name: "Luis",
+    subName: "Chávez",
+    age: 28,
+}
+let{name, subName} = person;
+console.log(name, subName); */
