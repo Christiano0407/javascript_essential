a1f62f79b3472565e689e4bc54ee8e738affde2e
Christiano0407 <placebo0704@gmail.com>
2020-11-27 09:40:07 -0600

    Math

--- src/js/objApp.js
+++ src/js/objApp.js
@@ -23,4 +23,26 @@ console.log(fecha.getTimezoneOffset());
 let cumple = new Date(1992,0,18);
 console.log(`Mi cumple es: ${cumple}`); */
 //
-//Object Math
+/* Object Math /Sus parámetros y Métodos: */
+/* //console.log(Math);
+//Math.abs = Devuelve el valor absoluto (No cambia).
+console.log(Math.abs(8.5));
+//Devuelve al número entero (Más cercano = Mayor)
+console.log(Math.ceil(7.5));
+// Devuelve el número entero (menor más cercano).
+console.log(Math.floor(6.4));
+//Redondear al más cercano inmediato (Menor o mayor).
+console.log(Math.round(3.6));
+console.log(Math.round(3.4));
+//Raíz cuadrada:
+console.log(Math.sqrt(25));
+//Potencia:
+console.log(Math.pow(2,3));
+//Valor positivo / negativo / cero
+console.log(Math.sign(5));
+//random = Valor aleatorio entre 0 y 1
+console.log(Math.random());
+console.log(Math.round(Math.random() * 10)); */
+
+//
+/* Operador de Cortocircuito */
