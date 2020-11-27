6137edc497eff5899482f6bf9a046b4a675d607d
Christiano0407 <placebo0704@gmail.com>
2020-11-27 09:14:27 -0600

    objects

--- src/js/objApp.js
+++ src/js/objApp.js
@@ -6,3 +6,21 @@
 //console.dir(document);
 //console.dir(window);
 //console.clear();
+
+//Object Date (constructor):
+//console.log(Date());
+//console.log(fecha.getDate());
+/*  let fecha = new Date();
+console.log(fecha.getMonth());
+console.log(fecha.getTime()); 
+//setTimeout (puedes usar).
+console.log(fecha.toLocaleTimeString());
+console.log(fecha.toLocaleDateString());
+console.log(fecha.getUTCHours());
+console.log(fecha.getUTCDate());
+console.log(fecha.getTimezoneOffset());
+//
+let cumple = new Date(1992,0,18);
+console.log(`Mi cumple es: ${cumple}`); */
+//
+//Object Math
