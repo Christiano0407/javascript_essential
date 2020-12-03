0c1cd4ce11f36f6030c4406f72508a6089748128
Christiano0407 <placebo0704@gmail.com>
2020-12-03 09:28:24 -0600

    moduloss

--- src/js/modul.js
+++ src/js/modul.js
@@ -1,5 +1,13 @@
+/* import {default as usuario, pasword} from "../js/modulApp" */
 //Necesraio que agreges "type=module".
 /* import {Pi} from "./modulApp";
 console.log(PI); */
-import {usuario, password} from "../js/modulApp";
-console.log(usuario);
+/* import {usuario, password} from "../js/";
+console.log("Hola");
+console.log(usuario); */
+/* import {person}from "./modulApp";
+import {saludar}from "./modulApp"; */
+//import {men}from "./modulApp";
+/* console.log(person); */
+/* saludar(); */
+//console.log(men);
--- src/js/modulApp.js
+++ src/js/modulApp.js
@@ -1,8 +1,20 @@
 /* const PI = Math.Pi;
 export default PI; */
 
-export let usuario = "Chris";
-export let pasword = "Pasword";
 //export{usuario, pasword};
 /* let usuario = "Chris";
 export default usuario; */
+
+ /* et usuario = "Chris";
+ let pasword = "Pasword";
+
+ export default {usuario,pasword } */
+/* export let men = {
+    name: "Chris",
+    apellido: "Velázquez",
+}; */
+
+ /* export let person = "Alma"; */
+ /* export default function saludar(){
+     console.log("Hola mundo!");
+ } */
