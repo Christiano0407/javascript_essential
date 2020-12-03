e3805ca2e42760cc8447c14f3180f8aea156a08c
Christiano0407 <placebo0704@gmail.com>
2020-12-03 12:16:25 -0600

    practice one

---
+++ src2/js/main.js
@@ -0,0 +1,43 @@
+//console.log("Hola Mundo");
+/* hola = () => {
+    let saludo = "Hola Mundo";
+    if(saludo === "Hola Mundo"){
+        console.log("Ella es una persona amable");
+    }else {
+        console.log("No lo es");
+    }
+
+    console.log(saludo.length);
+}
+hola();
+
+const NUM = 10;
+
+for(let i = 0; i <= NUM; i++){
+    if(i <= 5){
+        console.log("Reprobado");
+    }else if(i === 8 || i === 9){
+        console.log("Sólo son esta calificación van a pasar");
+    }else if(i === 10){
+        console.log("Excelencia académica");
+    }else {
+        console.log("Reprobados");
+    }
+    console.log(i); 
+} */
+
+/* const names = ["Alma", "Luisa", "Chris", "Mau", "Pamela"];
+console.log(names.length);
+console.log(names[1]); */
+/* for(let i = 0; i <= names.length; i++){
+    console.log(i); 
+} */
+//1)
+/*  function character(cadena=""){
+if(cadena === ""){
+    console.warn("No ingresaste ningún dato");
+}else {
+    console.info(`Ella dijo: ${cadena}, y tiene ${cadena.length}`);
+}
+}
+character("Hola Luisa");  */
