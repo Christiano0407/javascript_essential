5c49eda1f4752d5ee775b65270dabf104baf2095
Christiano0407 <placebo0704@gmail.com>
2020-12-03 08:06:18 -0600

    import and export

---
+++ src/js/modul.js
@@ -0,0 +1,5 @@
+//Necesraio que agreges "type=module".
+/* import {Pi} from "./modulApp";
+console.log(PI); */
+import {usuario, password} from "../js/modulApp";
+console.log(usuario);
---
+++ src/js/modulApp.js
@@ -0,0 +1,8 @@
+/* const PI = Math.Pi;
+export default PI; */
+
+export let usuario = "Chris";
+export let pasword = "Pasword";
+//export{usuario, pasword};
+/* let usuario = "Chris";
+export default usuario; */
