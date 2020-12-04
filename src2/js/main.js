a7ff8a5ae63e78f398ccb55d65f550be0bca338b
Christiano0407 <placebo0704@gmail.com>
2020-12-04 11:04:11 -0600

    nuevos ejercicios

--- src2/js/main.js
+++ src2/js/main.js
@@ -78,3 +78,45 @@ const friendsYears = (names = FRIEND, distance = undefined) =>
 
 friendsYears(console.log(FRIEND, " "));
 console.log(`Ella es la amiga de la familia:`,FRIEND[1]); */
+//4)Programa una función que repita un texto "X" veces.
+
+//Math.sign() - Retorna el signo de un número (positivo, negativo o 0).
+
+/* repetirTexto = (texto = "", veces= undefined) => {
+  if(texto === ""){
+    console.warn("No ingresaste ningún dato o valor")
+  }else if(veces === undefined){
+    console.warn("No ingresaste el número de veces a repetir el texto")
+  }else if(veces === 0){
+    console.error("El número de veces no puede ser 0")
+  }else if(Math.sign(veces) === -1){
+    console.error("El número no puede ser negativo")
+  }else {
+  console.log("Vuelve a repetir el procesos")
+  }
+
+  for(let i = 0; i <= veces; i++) {
+      console.info(`${texto}, ${i}`)
+  }
+}
+repetirTexto("Hola Mundo", 3); */
+/* repetirTexto("Hola", 0);
+repetirTexto("Saludo", -1); */
+/* oaracionNueva = (frase = "", repeticiones = undefined) => {
+  if(frase === ""){
+      console.log("No ingresaste ninguna frase o dato")
+  }else if(repeticiones === undefined){
+    console.log("Te faltó ingresar el número de veces que, necesitas.")
+  }else if(repeticiones === 0){
+   console.error("No es posible ingresar ese valor")
+  }else if(Math.sign(repeticiones) === -1){
+    console.error("El valor no puede ser negativo")
+  }else {
+   console.log("Te solicitamos volver a ingresar tus datos")
+  }
+
+  for(let i = 1; i <= repeticiones; i++) {
+      console.log(`${frase}, ${i}`)
+  }
+}
+oaracionNueva("Mi nombre es usuario", 3); */
