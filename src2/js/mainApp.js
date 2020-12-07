36131dbd7bcb11274ea2ee2145ac701184964bd4
Christiano0407 <placebo0704@gmail.com>
2020-12-07 08:52:25 -0600

    numeros

---
+++ src2/js/mainApp.js
@@ -0,0 +1,8 @@
+//Programa una función que obtenga un número aleatorio entre 500 a 600
+/* const numAleatorio = () => {
+    console.log(Math.round((Math.random()*100) + 500));
+}
+numAleatorio();
+//numAleatorio(); */
+
+//Capicúa(Se lee igual en un sentido u otro):
