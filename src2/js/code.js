680470dd7a96f017d87828b23761cb37f937c2ae
Christiano0407 <placebo0704@gmail.com>
2020-12-07 12:06:20 -0600

    asincronismo enjs

--- src2/js/code.js
+++ src2/js/code.js
@@ -1,2 +1,35 @@
-//Asincronismo en JS:
+      /* Asincronismo en JS: */
+      
 //Temporizadores:
+
+//1) setTimeout => Recibe uba función, y el tiempo / Una vez se ejecuta:
+/* console.log("inicio");
+setTimeout(() => {
+    console.log("Ejecutando...");
+}, 1000); */
+
+//2)SetInterval => Varias veces: 
+/* setInterval(() => {
+ console.log("Ejecutando varias veces");
+},1000); */
+
+//Actualizar la hora:
+/* setInterval(() => {
+    console.log(new Date().toLocaleTimeString());
+}, 1000);  */
+//limpiar un setTimeout:
+/* let temporizador = setTimeout(() => {
+   console.log(new Date().toLocaleTimeString());
+}, 1000);
+
+clearTimeout(temporizador); */
+
+/* EVENT LOOP */
+//Single thread y Multi Thread.
+//Operaciones en CPU (CRASHEA) y Operaciones I/O (Entrada y salida.
+//Operaciones bloqueantes y No bloqueantes.
+//Operaciones Síncronas y Asíncronas.+
+
+//Operaciones Síncronas Bloqueantes
+
+
