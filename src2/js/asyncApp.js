f948c605d36b98f3de97ee33d37f72a3dbd07fda
Christiano0407 <placebo0704@gmail.com>
2020-12-14 09:35:42 -0600

    fin-async

--- src2/js/asyncApp.js
+++ src2/js/asyncApp.js
@@ -1,4 +1,4 @@
-//Promise: 1:
+/* //Promise: 1:
 const somethingOne = () => {
     return new Promise((resolve, reject) => {
     (true)
@@ -14,10 +14,10 @@ const somethingTwo = async () => {
 }
 console.log(`BEFORE`);
 somethingTwo();
-console.log(`AFTER`);
+console.log(`AFTER`); */
 
 //Two)
-const OneAsync = () => {
+/* const OneAsync = () => {
     return new Promise((resolve, reject) => {
     (true)
     ? setTimeout(() => resolve(`Yes`),3000)
@@ -32,4 +32,25 @@ const twoAsync = async () => {
 
 console.log(`OneValue`);
 twoAsync();
-console.log(`TwoValue`);
+console.log(`TwoValue`); */
+
+//Three:
+/* const threeAsync = () => {
+    return new Promise((resolve, reject) => {
+     //(true)
+     (false)
+     ? setTimeout(() => resolve(`This is the way`), 5000)
+     :reject(new Error(`Not is the way`))
+    });
+}
+
+const fourAsync = async () => {
+  try {
+   const elseAsync = await threeAsync();
+   console.log(elseAsync);
+  }catch(error){
+   console.log(error);
+  }
+}
+console.log(`OneERROR`);
+fourAsync(); */
